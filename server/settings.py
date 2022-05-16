33403bcea52df66c54e181a316a5d5a1574d15be
alexreusc <alexreusc@gmail.com>
2022-05-16 15:53:09 -0500

    changed cors to allow all domains

--- server/settings.py
+++ server/settings.py
@@ -96,9 +96,11 @@ DATABASES = {
     }
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
-]
+CORS_ORIGIN_ALLOW_ALL = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000'
+# ]
 
 ##
 
